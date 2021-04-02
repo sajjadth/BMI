3966e44d1b51f1c69fb9ca4aa11aa0e4f704d640
sajjad <st.655.st@gmail.com>
2021-04-02 20:21:08 +0430

    project created

---
+++ README.md
@@ -0,0 +1,2 @@
+<h3>BMI</h3><br/>
+<p>Just a Daily Project</p>
---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.scss";
+import "bootstrap/dist/css/bootstrap.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="w-100 vh-100 d-flex justify-content-center align-items-center">
+          <h1>hello world!</h1>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
