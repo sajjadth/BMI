b097ca0f1ef9ac92e7a19eec61d90ab8ff16b1ff
sajjad <st.655.st@gmail.com>
2021-04-03 22:04:28 +0430

    normal calculation fixed

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ import React from "react";
 import "./style.scss";
 import "bootstrap/dist/css/bootstrap.css";
 import $ from "jquery";
-import Input from "./components/input";
-import Output from "./components/outout";
+import Normal from "./components/normal/main";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       bmi: 0,
+      advanced: false,
     };
     this.handleForm = this.handleForm.bind(this);
+  }
+  handleAdvance() {
+    this.setState({ advanced: true });
+  }
+  handleNormal() {
+    this.setState({ advanced: false });
   }
   handleForm(e) {
     e.preventDefault();
@@ -19,21 +25,72 @@ class App extends React.Component {
     const weight = Number($("#weight").val());
     const bmi = weight / ((height / 100) * (height / 100));
     if (bmi % 1 === 0) {
-      this.setState({ bmi: bmi });
+      this.setState({ bmi: Number(bmi) });
     } else {
       const bmiFixed = bmi.toFixed(1);
-      this.setState({ bmi: bmiFixed });
+      this.setState({ bmi: Number(bmiFixed) });
     }
   }
   render() {
     return (
       <React.Fragment>
         <div className="w-100 vh-100 d-flex flex-column justify-content-center align-items-center container">
-          <Input bmi={this.state.bmi} formControl={this.handleForm} />
-          <Output bmi={this.state.bmi} />
+          <div className="w-100 ml-5 d-flex flex-row justify-content-start align-items-center mt-2">
+            <input
+              type="button"
+              className="btn btn-secondary m-0"
+              id="advance-btn"
+              value="advance"
+              onClick={() => this.handleAdvance()}
+            />
+            <input
+              type="button"
+              className="btn btn-secondary m-0"
+              id="normal-btn"
+              value="normal"
+              onClick={() => this.handleNormal()}
+            />
+          </div>
+          {this.state.advanced === false ? (
+            <Normal formControl={this.handleForm} bmi={this.state.bmi} />
+          ) : (
+            console.log(50)
+          )}
         </div>
       </React.Fragment>
     );
+  }
+  componentDidMount() {
+    const normal = $("#normal-btn");
+    const advance = $("#advance-btn");
+
+    this.state.advanced === false
+      ? normal.removeClass("btn-secondary") &&
+        normal.addClass("btn-light") &&
+        advance.addClass("btn-secondary") &&
+        advance.removeClass("btn-light")
+      : this.state.advanced === true
+      ? advance.addClass("btn-light") &&
+        advance.removeClass("btn-secondary") &&
+        normal.addClass("btn-secondary") &&
+        normal.removeClass("btn-light")
+      : null;
+  }
+  componentDidUpdate() {
+    const normal = $("#normal-btn");
+    const advance = $("#advance-btn");
+
+    this.state.advanced === false
+      ? normal.removeClass("btn-secondary") &&
+        normal.addClass("btn-light") &&
+        advance.addClass("btn-secondary") &&
+        advance.removeClass("btn-light")
+      : this.state.advanced === true
+      ? advance.addClass("btn-light") &&
+        advance.removeClass("btn-secondary") &&
+        normal.addClass("btn-secondary") &&
+        normal.removeClass("btn-light")
+      : null;
   }
 }
 
--- src/components/normal/input.jsx
+++ src/components/normal/input.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import $ from "jquery";
 
 class Input extends React.Component {
   constructor(props) {
@@ -10,14 +9,14 @@ class Input extends React.Component {
     const { formControl } = this.props;
     return (
       <React.Fragment>
-        <div className="w-100 text-center">
+        <div className="w-100 d-flex justify-content-center align-items-center" id="input">
           <form
             className="d-flex flex-sm-row flex-column justify-content-center align-items-center"
-            onSubmit={(e) => this.props.formControl(e)}
+            onSubmit={(e) => formControl(e)}
           >
             <input
               type="number"
-              className="form-control m-1 w-100"
+              className="form-control m-2 w-100"
               id="height"
               placeholder="height cm"
               min="100"
@@ -25,14 +24,14 @@ class Input extends React.Component {
             />
             <input
               type="number"
-              className="form-control m-1 w-100"
+              className="form-control m-2 w-100"
               id="weight"
               placeholder="weight"
               required
             />
             <input
               type="submit"
-              className="btn btn-success m-1 w-100"
+              className="btn btn-success m-2 w-100"
               id="submit"
               value="submit"
             />
